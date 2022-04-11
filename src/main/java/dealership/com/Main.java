915f9e8982eb4a88e568be7788da65d983920c8e
tomeksergiel279 <76492072+tomeksergiel279@users.noreply.github.com>
2022-04-11 10:44:57 +0200

    Update Main.java

--- src/main/java/dealership/com/Main.java
+++ src/main/java/dealership/com/Main.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Main {
@@ -13,20 +12,5 @@ public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class);
-    }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/client").allowedOrigins(LOCALHOST);
-                registry.addMapping("/car").allowedOrigins(LOCALHOST);
-                registry.addMapping("/department").allowedOrigins(LOCALHOST);
-                registry.addMapping("/buy").allowedOrigins(LOCALHOST);
-                registry.addMapping("/employee").allowedOrigins(LOCALHOST);
-                registry.addMapping("/servicebooklet").allowedOrigins(LOCALHOST);
-            }
-        };
     }
 }
