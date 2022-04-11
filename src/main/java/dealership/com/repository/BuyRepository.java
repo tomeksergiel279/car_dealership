d0d3b914e1e8d00143c6865abecbef9e56afe224
tomeksergiel279 <76492072+tomeksergiel279@users.noreply.github.com>
2022-04-11 14:11:14 +0200

    Delete src directory

--- src/main/java/dealership/com/repository/BuyRepository.java
+++
@@ -1,7 +0,0 @@
-package dealership.com.repository;
-
-import dealership.com.model.Buy;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BuyRepository extends JpaRepository<Buy, Long> {
-}
--- src/main/java/dealership/com/repository/ServiceBookletRepository.java
+++
@@ -1,7 +0,0 @@
-package dealership.com.repository;
-
-import dealership.com.model.ServiceBooklet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServiceBookletRepository extends JpaRepository<ServiceBooklet, Long> {
-}
